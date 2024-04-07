ccd24a1ca2d14cbb45a84a4b46a5aaaaddc5a2d1
Himashreems <x23189851@student.ncirl.ie>
2024-04-07 14:30:50 +0100

    changes in the code

--- room_slot/settings.py
+++ room_slot/settings.py
@@ -126,18 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
 
+# Media Files Uploaded by Users
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'www', 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
