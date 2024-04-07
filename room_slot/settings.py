ced6099711d5ba6ea190e81c3b967b4b64138fbd
Himashreems <x23189851@student.ncirl.ie>
2024-04-07 13:04:21 +0100

    added mysql connetioss

--- room_slot/settings.py
+++ room_slot/settings.py
@@ -85,7 +85,11 @@ DATABASES = {
         'USER': 'admin',
         'PASSWORD': 'password',
         'HOST': 'x23189851-devsecops-dev1.cxqospjboyrv.eu-west-2.rds.amazonaws.com',
-        'PORT': '3306'
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
 }
 
 }
